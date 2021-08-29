b3ce12e21967c67831053089bcc3fb6cb9da3a14
Geovanni Gonzalez <32276348+GeovanniG@users.noreply.github.com>
2021-08-29 15:52:18 -0700

    Update header interests

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -7,14 +7,14 @@ const Header = () => {
                 <span className="home-header__title--inverse">Geo</span>vanni Gonzalez
             </h1>
             <div className="home-header__text--fade-in">
-                <p>A Software Quality Analyst Engineer</p>
+                <p>A Software Engineer</p>
                 <p>with interests in</p>
-                <p className="home-header__text--fade-in-50ms"><span className="home-header__text--color">Automation</span>,</p>
+                <p className="home-header__text--fade-in-50ms"><span className="home-header__text--color">Backend Development</span>,</p>
                 <p className="home-header__text--fade-in-100ms">
-                    <span className="home-header__text--color">Testing</span>, 
+                    <span className="home-header__text--color">Machine Learning</span>, 
                     <span className="home-header__text--fade-in-150ms">and also</span>
                 </p>
-                <p className="home-header__text--fade-in-200ms">Software <span className="home-header__text--color">Development</span>.</p>
+                <p className="home-header__text--fade-in-200ms">Artificial <span className="home-header__text--color">Intelligence</span>.</p>
             </div>
         </header>
     )
