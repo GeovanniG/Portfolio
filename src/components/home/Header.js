fd0998f057fd6c8172c544851b0b38cd0b9c8838
Geovanni Gonzalez <32276348+GeovanniG@users.noreply.github.com>
2021-08-29 16:25:03 -0700

    Improve header text color and spacing

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -11,10 +11,10 @@ const Header = () => {
                 <p>with interests in</p>
                 <p className="home-header__text--fade-in-50ms"><span className="home-header__text--color">Backend Development</span>,</p>
                 <p className="home-header__text--fade-in-100ms">
-                    <span className="home-header__text--color">Machine Learning</span>, 
-                    <span className="home-header__text--fade-in-150ms">and also</span>
+                    <span className="home-header__text--color">Machine Learning</span>,
+                    <span className="home-header__text--fade-in-150ms"> and also</span>
                 </p>
-                <p className="home-header__text--fade-in-200ms">Artificial <span className="home-header__text--color">Intelligence</span>.</p>
+                <p className="home-header__text--fade-in-200ms home-header__text--color">Artificial Intelligence.</p>
             </div>
         </header>
     )
